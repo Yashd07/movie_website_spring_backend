50601159ac1b05c273e592f8f39b8c51afd8ae37
Yashd07 <yashd1272@gmail.com>
2023-12-09 20:41:18 +0530

    final commit

---
+++ src/test/java/dev/yashdesai/springmovies/SpringmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yashdesai.springmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
